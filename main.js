b26606876e04ae3ee9bec9d51224a7642e2407cc
Artur Babiński <artur.babinski.g@gmail.com>
2020-09-24 13:50:51 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+alert('siema! :D');
+console.log('Siema!');
+
+const name = 'Artur';
+const age = 25 ;
+
+console.log(name);
+console.log(age);
+
+console.log(`Cześć! Nazywam się ${name} i mam ${age} lat`);
